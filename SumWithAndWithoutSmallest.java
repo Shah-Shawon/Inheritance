54af7cc12e3a571d50d8d0facf444a88c267b832
Shah-Shawon <107519139+Shah-Shawon@users.noreply.github.com>
2022-10-12 22:06:45 +0600

    Add files via upload

---
+++ SumWithAndWithoutSmallest.java
@@ -0,0 +1,17 @@
+public class SumWithAndWithoutSmallest {
+    public static void main(String[] args) {
+        int[] numbers= {12,11,15,37,65,80,22,98,30,15};
+        int smallest,sum=0,diff,size;
+        size=numbers.length;
+        smallest=numbers[0];
+        for(int i=1;i<size;i++){
+            if(numbers[i]<smallest){
+                sum=sum+smallest;
+                smallest=numbers[i];
+            }
+            else sum=sum+numbers[i];
+        }
+        diff=sum-smallest;
+        System.out.println("Difference of Sum and the smallest number = "+diff);
+    }
+}
